2a73b7f620f838087a8f14e07624934502ea84af
David L. Inman <davidlevere@gmail.com>
2019-02-19 00:29:15 -0500

    stubbing out vec.h

--- vec.h
+++ vec.h
@@ -1,3 +1,12 @@
 
 #define vec_def(type) \
+struct vec_##type { \
+    size_t capacity; \
+    size_t count; \
+    type * items; \
+}; \
+   \
+void vec_##type##_push( vec_##type * v, type i ); \
+void vec_##type##_init( vec_##type * v ); \
+void vec_##type##_delete( vec_##type * v ); \
 
