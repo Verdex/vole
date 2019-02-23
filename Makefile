724294a71ef9e0319ec76eb3eddc05034c94604f
David L. Inman <davidlevere@gmail.com>
2019-02-23 16:01:59 -0500

    inc prog for test

--- Makefile
+++ Makefile
@@ -1,2 +1,13 @@
 
+test: test_vec
+	./test_vec
 
+
+test_vec: 
+	clang test_vec.c -o test_vec
+
+
+clean: 
+	rm -rf *.o 
+	rm -rf test_vec 
+	rm -rf a.out 
--- test_vec.c
+++ test_vec.c
@@ -1,37 +1,68 @@
 
 #include "vec.h"
 #include<stdbool.h>
+#include<stdint.h>
 #include<stdlib.h>
 #include<string.h>
+#include<assert.h>
 
+static void** item_alloc_ptr;
+static size_t* item_alloc_size;
+static int item_alloc_call_count;
 static void* local_item_alloc(size_t size) {
-    return NULL;
+    void* ret = item_alloc_ptr[item_alloc_call_count];
+    item_alloc_size[item_alloc_call_count] = size;
+    item_alloc_call_count++;
+    return ret;
 }
 
+static void** item_free_ptr;
+static int item_free_call_count;
 static void local_item_free(void* p) {
-
+    item_free_ptr[item_free_call_count] = p;
+    item_free_call_count++;
 }
 
+static void** vec_alloc_ptr;
+static int vec_alloc_call_count;
 static void* local_vec_alloc() {
-    return NULL;
+    void* ret = vec_alloc_ptr[vec_alloc_call_count];
+    vec_alloc_call_count++;
+    return ret;
 }
 
+static void** vec_free_ptr;
+static int vec_free_call_count;
 static void local_vec_free(void* p) {
-
+    vec_free_ptr[vec_free_call_count] = p;
+    vec_free_call_count++;
 }
 
+static void** stable_alloc_ptr;
+static int stable_alloc_call_count;
 static void* local_stable_alloc() {
-    return NULL;
+    void* ret = stable_alloc_ptr[stable_alloc_call_count];
+    stable_alloc_call_count++;
+    return ret;
 }
 
+static void** stable_free_ptr;
+static int stable_free_call_count;
 static void local_stable_free(void* p) {
-
+    stable_free_ptr[stable_free_call_count] = p;
+    stable_free_call_count++;
 }
 
 vec_def(int)
 
 vec_code(int)
 
+void should_handle_with_capacity() {
+
+    vec_alloc_call_count = 0;
+
+    vec_int* vec = vec_int_with_capacity(10);
+}
 
 int main() {
 
