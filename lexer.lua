735224dfa50b9e22b9c3d882718aaecb921cf7a9
David L. Inman <davidlevere@gmail.com>
2019-03-08 20:22:12 -0500

    symbol rule

--- lexer.lua
+++ lexer.lua
@@ -38,8 +38,16 @@ local regex_match = string.match
 local function match_char(c) return function(o) return o == c end end
 local function start_symbol_char(c) return regex_match(c, '[a-zA-Z_]') end
 local function rest_symbol_char(c) return regex_match(c, '[a-zA-Z_0-9]') end
+local concat = table.concat
+local function concat_matches(ms)
+    local t = {}
+    for _, v in ipairs( ms ) do
+        t[#t+1] = v.text
+    end
+    return concat( t )
+end
 
-local lang_def = rule_engine.def {
+local lex_def = rule_engine.def {
     match( 'comma', match_char( ',' ) ),
     match( 'semi', match_char( ';' ) ),
     match( 'colon', match_char( ':' ) ),
@@ -53,20 +61,49 @@ local lang_def = rule_engine.def {
     match( 'start_symbol_char', start_symbol_char ),
     match( 'rest_symbol_char', rest_symbol_char ),
 
+    rule( 'symbol', { m 'start_symbol_char'
+                    , zero_or_more( m 'rest_symbol_char' ) 
+                    }, 
+                    function (x) return { name = "symbol"
+                                        , index = x[1].index
+                                        , value = x[1].text .. concat_matches( x[2] ) 
+                                        } end ),
 
-    rule( 'main', { m 'comma' }, function (x) return x[1] end ),
+    rule( 'main', { zero_or_more( alt{ m 'comma'
+                                     , m 'semi'
+                                     , m 'colon'
+                                     , m 'l_square'
+                                     , m 'r_square'
+                                     , m 'l_paren'
+                                     , m 'r_paren'
+                                     , m 'l_curly'
+                                     , m 'r_curly'
+                                     , m 'dot'
+                                     , r 'symbol'
+                                     } ) 
+                  }, 
+                  function (x) return x[1] end ),
 }
 
-
-local function parse( input )
+local function lex( input )
     local sub = string.sub
     local at = function ( s, i ) return sub(s, i, i) end
-    local success, index, output = rule_engine.eval(lang_def, input, at)
+    local success, index, output = rule_engine.eval(lex_def, input, at)
     if not success then
         return false, err.error_at(input, index) 
     end
     return true, output
 end
 
-x, v = parse(',')
+x, v = lex(',,;:{.}[]()blah')
+
+if x then 
+    print "success"
+    for _,vlet in ipairs( v ) do
+        print( vlet.index, vlet.text, vlet.name )
+    end
+else
+    print "failure"
+    print( v )
+end
 
