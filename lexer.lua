1f2c34b29870811135d920170f4ba3b05ee1596f
David L. Inman <davidlevere@gmail.com>
2019-03-10 20:15:12 -0400

    line comment

--- lexer.lua
+++ lexer.lua
@@ -40,6 +40,7 @@ local function start_symbol_char(c) return regex_match(c, '[a-zA-Z_]') end
 local function rest_symbol_char(c) return regex_match(c, '[a-zA-Z_0-9]') end
 local function digit(c) return regex_match(c, '[0-9]') end
 local function whitespace(c) return regex_match(c, '%s') end
+local function anything_but_endline(c) return regex_match(c, '[^\r\n]') end
 local concat = table.concat
 local function concat_matches(ms)
     local t = {}
@@ -65,6 +66,20 @@ local lex_def = rule_engine.def {
     match( 'rest_symbol_char', rest_symbol_char ),
     match( 'digit', digit ), 
     match( 'whitespace', whitespace ),
+    match( 'div', match_char( '/' ) ),
+    match( 'star', match_char( '*' ) ),
+    match( 'new_line', match_char( '\n' ) ),
+    match( 'carriage_return', match_char( '\r' ) ),
+    match( 'anything_but_endline', anything_but_endline ), 
+
+    rule( 'line_comment', { m 'div'
+                          , m 'div' 
+                          , zero_or_more( m 'anything_but_endline' )
+                          , alt { m 'carriage_return', m 'new_line' }
+                          }, function(x) return { name = "comment" } end  ),
+
+    --rule( 'line_commnet_eof', ),
+    --rule( 'block_comment', ),
 
     rule( 'symbol', { m 'start_symbol_char'
                     , zero_or_more( m 'rest_symbol_char' ) 
@@ -97,6 +112,7 @@ local lex_def = rule_engine.def {
                                      , r 'number'
                                      , m 'whitespace'
                                      , m 'equal'
+                                     , r 'line_comment'
                                      } ) 
                   }, 
                   function (x) return x[1] end ),
@@ -111,7 +127,7 @@ local function lex( input )
     end
     local ret = {}
     for _, v in ipairs( output ) do
-        if v.name ~= 'whitespace' then
+        if v.name ~= 'whitespace' and v.name ~= 'comment' then
             ret[#ret+1] = v
         end
     end
@@ -119,7 +135,8 @@ local function lex( input )
 end
 
 x, v = lex([[,,;:{.}[]( ) 8 89
-
+// blah
+//
 =
 10.1048
 
